0ef7797caac6064a8f8db18cf7fe801c46811c1d
doublejkim <jaejin.km@gmail.com>
2023-04-18 18:51:56 +0900

    커스텀 어노테이션이 존재하는지 Reflection 을 통해확인하여 객체를 주입하도록 변경

--- src/me/doublej/reflectionadv/ContainerService.java
+++ src/me/doublej/reflectionadv/ContainerService.java
@@ -1,11 +1,35 @@
 package me.doublej.reflectionadv;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 
 public class ContainerService {
 
+    /*
+    @Inject 어노테이션이 붙어있으면 동일하게 인스턴스를 만들어서 주입하도록 변경
+     */
     public static <T> T getObject(Class<T> classType)  {
-        return createInstance(classType);
+
+        T instance = createInstance(classType);
+        Arrays.stream(classType.getDeclaredFields()).forEach(
+                f -> {
+                    Inject annotation = f.getAnnotation(Inject.class);
+                    if (annotation != null) {
+                        Object filedInstance = createInstance(f.getType());
+                        f.setAccessible(true);  // field 는 private/default 인 경우가 많기때문에..
+
+                        try {
+
+                            f.set(instance, filedInstance);
+
+                        } catch (IllegalAccessException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        );
+
+        return instance;
     }
 
     /*
